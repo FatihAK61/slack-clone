da9b153741f7bf8cf809a28dd3d501fcc2881273
FatihAK61 <fatihak61@gmail.com>
2025-08-24 22:14:44 +0300

    Update

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,18 +1,18 @@
-import { Inngest } from "inngest";
-import { connectDB } from "./db.js";
-import { User } from "../models/user.model.js"; // Import the User model
-import { addUserToPublicChannels, deleteStreamUser, upsertStreamUser } from "./stream.js";
+import {Inngest} from "inngest";
+import {connectDB} from "./db.js";
+import {User} from "../models/user.model.js"; // Import the User model
+import {addUserToPublicChannels, deleteStreamUser, upsertStreamUser} from "./stream.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "slack-clone" });
+export const inngest = new Inngest({id: "slack-clone"});
 
 const syncUser = inngest.createFunction(
-    { id: "sync-user" },
-    { event: "clerk/user.created" },
-    async ({ event }) => {
+    {id: "sync-user"},
+    {event: "clerk/user.created"},
+    async ({event}) => {
         await connectDB();
 
-        const { id, email_addresses, first_name, last_name, image_url } = event.data;
+        const {id, email_addresses, first_name, last_name, image_url} = event.data;
 
         const newUser = {
             clerkId: id,
@@ -34,12 +34,12 @@ const syncUser = inngest.createFunction(
 );
 
 const deleteUserFromDB = inngest.createFunction(
-    { id: "delete-user-from-db" },
-    { event: "clerk/user.deleted" },
-    async ({ event }) => {
+    {id: "delete-user-from-db"},
+    {event: "clerk/user.deleted"},
+    async ({event}) => {
         await connectDB();
-        const { id } = event.data;
-        await User.deleteOne({ clerkId: id });
+        const {id} = event.data;
+        await User.deleteOne({clerkId: id});
 
         await deleteStreamUser(id.toString());
     }
