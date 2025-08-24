1cc05ca330f048697185f3b85e290e50beaa6d65
FatihAK61 <fatihak61@gmail.com>
2025-08-24 19:43:44 +0300

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useAuth} from "@clerk/clerk-react";
+import {Navigate, Route, Routes} from "react-router";
 
-function App() {
-  const [count, setCount] = useState(0)
+import AuthPage from "./pages/AuthPage";
+import CallPage from "./pages/CallPage";
+import HomePage from "./pages/HomePage";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import * as Sentry from "@sentry/react";
 
-export default App
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
+
+const App = () => {
+    const {isSignedIn, isLoaded} = useAuth();
+
+    if (!isLoaded) return null;
+
+    return (
+        <SentryRoutes>
+            <Route path="/" element={isSignedIn ? <HomePage/> : <Navigate to={"/auth"} replace/>}/>
+            <Route path="/auth" element={!isSignedIn ? <AuthPage/> : <Navigate to={"/"} replace/>}/>
+
+            <Route
+                path="/call/:id"
+                element={isSignedIn ? <CallPage/> : <Navigate to={"/auth"} replace/>}
+            />
+
+            <Route
+                path="*"
+                element={isSignedIn ? <Navigate to={"/"} replace/> : <Navigate to={"/auth"} replace/>}
+            />
+        </SentryRoutes>
+    );
+};
+
+export default App;
